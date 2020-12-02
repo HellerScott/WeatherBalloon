d27c52ccbed9afff338db18acc04eb252360f80e
Justin <ultrasteele@gmail.com>
2020-12-02 13:29:54 -0700

    Create main.ino
    rudimentary main file

---
+++ main.ino
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  // call preflight functions - 
+  // check air preasure, GPS location and direction facing, save in memory
+
+
+  // call rising functions - 
+  // keep checking airpreassure and facing direction to ensure rising data
+
+  // call falling functions - 
+  // upon balloon detatch -> immediately save GPS location and direction facing (in degrees) then compare that info to original location, decide on course of action, take that action and turn off for 3 to 5 seconds, loop again.
+
+}
