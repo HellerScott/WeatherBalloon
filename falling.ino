8582f20ceb9800ad084c3ab3cc3ae9ec7c96af45
Justin <ultrasteele@gmail.com>
2020-12-02 13:31:38 -0700

    Create falling.ino

---
+++ falling.ino
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int falling() {
+  // falling functions - 
+  // upon balloon detatch -> immediately save GPS location and direction facing (in degrees) then compare that info to original location, decide on course of action, take that action and turn off for 3 to 5 seconds, loop again.
+
+}
